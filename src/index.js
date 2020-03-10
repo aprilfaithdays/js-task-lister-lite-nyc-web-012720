c5b6a170eb82005c8f47ace4e077e3fc8712f9f8
aprilfaithdays <aprilfaith.e@gmail.com>
2020-03-10 18:52:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // when button is clicked, get the user's input
+  // event listener is 'click'
+  // add a line inside "My Todos" section under the ul tag with the user's input
+  
+  let button = document.getElementsByTagName('input')[1];
+
+  
+  button.addEventListener('click', function(event){
+    event.preventDefault()
+
+    let listToUpdate = document.getElementById("tasks");
+    let text = document.querySelector("input").value;
+  
+    function userInput(text) {
+        let li = document.createElement('li')
+        li.innerHTML =`${text} 
+        <button data-description= ${text} >X</button>`
+      return li;
+    }
+  
+    listToUpdate.appendChild(userInput(text));
+
+  })
+
 });
